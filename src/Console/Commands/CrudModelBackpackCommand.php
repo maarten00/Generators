1ef1f8825f8f313dcb08d84e96bf972bff2dbfdb
Cristian Tabacitu <cristitabacitu@gmail.com>
2021-01-11 15:35:06 -0400

    use yellow output when nothing is done

--- src/Console/Commands/CrudModelBackpackCommand.php
+++ src/Console/Commands/CrudModelBackpackCommand.php
@@ -96,7 +96,7 @@ class CrudModelBackpackCommand extends GeneratorCommand
             // check if it already uses CrudTrait
             // if it does, do nothing
             if (Str::contains($file, $this->crudTrait)) {
-                $this->info('Model already used CrudTrait.');
+                $this->comment('Model already used CrudTrait.');
 
                 return;
             }
